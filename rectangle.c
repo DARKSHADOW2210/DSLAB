3764adf437bdc56974ff61ff7c8b0732a4b99e2a
darkshadow2210 <23911a1225vijayguttula@gmail.com>
2024-12-19 08:26:13 +0530

    Update rectangle.c

--- rectangle.c
+++ rectangle.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 struct Rectangle {
-	float length;
-float width;
+    float length;
+    float width;
 };
-int calcrec(struct Rectangle rect){
-	float area = rect.length*rect.width;
-	return area;
+
+float calcrec(struct Rectangle rect) {
+    return rect.length * rect.width;
 }
 
 int main() {
-	struct Rectangle rect;
-	printf("Enter the length of the rectangle: ");
-	scanf("%f",&rect.length);
-	printf("Enter the width of the rectangle: ");
-	scanf("%f",&rect.width);
-	float area = rect.length*rect.width;
-	printf("The area of the rectangle is: %.2f\n",area);
+    struct Rectangle rect;
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &rect.length);
+    printf("Enter the width of the rectangle: ");
+    scanf("%f", &rect.width);
+    float area = calcrec(rect);
+    printf("The area of the rectangle is: %.2f\n", area);
+    return 0;
 }
