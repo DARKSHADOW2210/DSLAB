90c020cbaca24ec48e3acd75d4ef62462e5dc014
darkshadow2210 <23911a1225vijayguttula@gmail.com>
2024-12-19 08:35:18 +0530

    Update distances.c

--- distances.c
+++ distances.c
@@ -9,7 +9,7 @@ struct Distance addD(struct Distance d1, struct Distance d2){
 	result.inches = d1.inches+d2.inches;
 	if(result.inches>=12.0){
 		result.feet += (int)(result.inches/12);
-		result.inches = (int)result.inches%12;
+		result.inches = (int)(result.inches%12);
 	}
 	return result;
 }
